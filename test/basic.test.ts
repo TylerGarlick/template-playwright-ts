ab56c903a42e295322fb8df2f71e0a336d9e6a0e
Tyler Garlick <136096+TylerGarlick@users.noreply.github.com>
2021-10-19 15:09:13 +0000

    Additions to the template

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,1 +1,23 @@
-it(`should work`, () => expect(true).toBeTruthy())
+const { chromium } = require('playwright')
+
+let browser
+let page
+
+beforeAll(async () => {
+  browser = await chromium.launch()
+})
+afterAll(async () => {
+  await browser.close()
+})
+beforeEach(async () => {
+  page = await browser.newPage()
+})
+afterEach(async () => {
+  await page.close()
+})
+
+
+it('should work', async () => {
+  await page.goto('https://www.google.com/')
+  expect(await page.title()).toBe('Google')
+})
